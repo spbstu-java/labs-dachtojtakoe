f2bd748976f84088a62c137be0a5e78e2b5bc8f8
dachtojtakoe <78024557+dachtojtakoe@users.noreply.github.com>
2025-09-30 09:32:20 +0300

    Решение лабораторной работы 4

--- lab2/Repeat.java
+++
@@ -1,8 +0,0 @@
-package lab2;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Repeat {
-    int value() default 1;
-}
