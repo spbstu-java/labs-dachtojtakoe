e39830fb2d5d15caf287c585f19502928a41c05e
dachtojtakoe <78024557+dachtojtakoe@users.noreply.github.com>
2025-10-01 10:31:14 +0300

    Убрал ненужное использование optionaldouble

--- lab4/StreamClass.java
+++ lab4/StreamClass.java
@@ -4,8 +4,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamClass {
-    public static OptionalDouble getAverage(List<Integer> numbers){
-        return numbers.stream().mapToInt(Integer::intValue).average();
+    public static double getAverage(List<Integer> numbers){
+        return numbers.stream().mapToInt(Integer::intValue).average().orElse(0.0);
     }
 
     public static List<String> strToUpperPrefix(List<String> strings){
